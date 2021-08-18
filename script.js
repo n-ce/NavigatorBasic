6a7d871f1d339fe16bd23f7c42b7f67bb56482d2
n-ce <abrocks1120@gmail.com>
2021-08-18 23:13:58 +0530

    Refinements

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 var p = document.getElementById('Point').style;
 var x=0;
 var y=0;
-document.getElementById('left').onclick = function() {
+function left() {
   x++;
   p.transform = "translate(" + -x+"rem,"+-y + "rem)";
   return x;
 }
-document.getElementById('right').onclick = function() {
+function right() {
   x--;
   p.transform = "translate(" + -x+"rem,"+-y + "rem)";
   return x;
 }
-document.getElementById('up').onclick = function() {
+function up() {
   y++;
   p.transform = "translate(" + -x+"rem,"+-y+ "rem)";
   return y;
 }
-document.getElementById('down').onclick = function() {
+function down() {
   y--;
   p.transform = "translate(" + -x+"rem,"+-y + "rem)";
   return y;
 }
-document.body.addEventListener("click", function(){
-  document.querySelector('h3').innerHTML=x+","+y;});
+document.body.onclick = function(){
+  var h = document.querySelector('h3');
+  h.innerHTML=x+","+y;}

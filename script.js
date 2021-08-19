56d0e372cec259685a03a802bafdb0ba11ce7b61
n-ce <abrocks1120@gmail.com>
2021-08-19 08:19:32 +0530

    button unintended selection fixed

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 var p = document.getElementById('Point').style;
 var x=0;
 var y=0;
-function left() {
+function right() {
   x++;
-  p.transform = "translate(" + -x+"rem,"+-y + "rem)";
+  p.transform = "translate(" + x+"rem,"+-y + "rem)";
   return x;
 }
-function right() {
+function left() {
   x--;
-  p.transform = "translate(" + -x+"rem,"+-y + "rem)";
+  p.transform = "translate(" + x+"rem,"+-y + "rem)";
   return x;
 }
 function up() {
   y++;
-  p.transform = "translate(" + -x+"rem,"+-y+ "rem)";
+  p.transform = "translate(" + x+"rem,"+-y+ "rem)";
   return y;
 }
 function down() {
   y--;
-  p.transform = "translate(" + -x+"rem,"+-y + "rem)";
+  p.transform = "translate(" + x+"rem,"+-y + "rem)";
   return y;
 }
-document.body.onclick = function(){
-  var h = document.querySelector('h3');
-  h.innerHTML=x+","+y;}
+document.body.onclick = function(){document.querySelector('h3').innerHTML=x+","+y;}
